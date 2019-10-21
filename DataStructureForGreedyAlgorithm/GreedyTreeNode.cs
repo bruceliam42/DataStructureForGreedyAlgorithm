e53ed9bf4d315778fb8bfae565b287bd76f28d35
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-10-21 19:03:06 -0400

    First Commit

---
+++ DataStructureForGreedyAlgorithm/GreedyTreeNode.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureForGreedyAlgorithm
+{
+    /// <summary>
+    /// Model a node in a tree structure
+    /// </summary>
+    class GreedyTreeNode
+    {
+        private List<GreedyTreeNode> greedyTreeNodes;
+        private int mCost;
+
+        /// <summary>
+        /// The cost to get to this node if you are traversing the tree
+        /// </summary>
+        public int cost {
+            get { return mCost; }
+            set { mCost = value; }
+        }
+        /// <summary>
+        /// Find the cheapest route out of this node
+        /// </summary>
+        /// <returns>A reference to the cheapest node connected to this node</returns>
+        public GreedyTreeNode FindCheapestRoute()
+        {
+            int minCost = 0;
+            GreedyTreeNode cheapestGreedyTreeNode = null;
+            foreach (GreedyTreeNode greedyTreeNode in greedyTreeNodes)
+            {
+                if (greedyTreeNode.cost < minCost)) {
+                    minCost = greedyTreeNode.cost;
+                    cheapestGreedyTreeNode = greedyTreeNode;    // Copy the reference
+                };
+            }
+            return cheapestGreedyTreeNode;
+        }
+        public void addGreedyTreeNode(GreedyTreeNode greedyTreeNode)
+        {
+            greedyTreeNodes.Add(new GreedyTreeNode(greedyTreeNode));
+        }
+        /// <summary>
+        /// Copy Constructor
+        /// </summary>
+        /// <param name="greedyTreeNode"></param>
+        public GreedyTreeNode(GreedyTreeNode NewGeedyTreeNode)
+        {
+            this.mCost = NewGeedyTreeNode.cost;
+            greedyTreeNodes = new List<GreedyTreeNode>();
+            foreach (GreedyTreeNode greedyTreeNode in NewGeedyTreeNode.GetGreedyTreeNodes())
+            {
+                this.addGreedyTreeNode(greedyTreeNode);
+            }
+        }
+        /// <summary>
+        /// Root Node Constructor
+        /// </summary>
+        /// <param name="cost"></param>
+        public GreedyTreeNode ()
+        {
+            this.cost = 0;
+            greedyTreeNodes = new List<GreedyTreeNode>();
+        }
+        /// <summary>
+        /// Get the list of nodes connected to this node
+        /// </summary>
+        /// <returns>The list of nodes connected to this node.</returns>
+        public List<GreedyTreeNode> GetGreedyTreeNodes() { return greedyTreeNodes; }
+    }
+}
--- DataStructureForGreedyAlgorithm/Program.cs
+++ DataStructureForGreedyAlgorithm/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,9 @@ namespace DataStructureForGreedyAlgorithm
     {
         static void Main(string[] args)
         {
+            // Create a root node. The cost is not relevant.
+            GreedyTreeNode myTree = new GreedyTreeNode();
+            // Add more nodes...
         }
     }
 }
